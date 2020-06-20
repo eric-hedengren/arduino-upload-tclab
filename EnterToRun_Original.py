5701dc52bb30d24bbbec87528e3a94b138fdd01b
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-06-20 12:33:54 -0600

    Update EnterToRun_Original.py

--- EnterToRun_Original.py
+++ EnterToRun_Original.py
@@ -8,4 +8,4 @@ n2 = '6'
 while input('Press enter to upload ') == '':
     Serial('COM'+n,baudrate=1200).close()
     sleep(1)
-    run(["C:\\Progra~2\\Arduino\\hardware\\tools\\avr/bin/avrdude", "-CC:\\Progra~2\\Arduino\\hardware\\tools\\avr/etc/avrdude.conf", "-v", "-patmega32u4", "-cavr109", "-PCOM"+n2, "-b57600", "-D", "-Uflash:w:C:\\Users\\Hedengren\\Desktop\\Python\\tclab_v2/tclab_v2.ino.hex:i"])
+    run(["C:\\Progra~2\\Arduino\\hardware\\tools\\avr/bin/avrdude", "-CC:\\Progra~2\\Arduino\\hardware\\tools\\avr/etc/avrdude.conf", "-v", "-patmega32u4", "-cavr109", "-PCOM"+n2, "-b57600", "-D", "-Uflash:w:tclab_v2.ino.hex:i"])
