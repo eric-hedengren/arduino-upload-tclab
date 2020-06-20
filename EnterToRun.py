cdbcce9eed3a1abae63d1343dc791f5bd84057f7
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-06-20 12:17:35 -0600

    Add files via upload

--- EnterToRun.py
+++ EnterToRun.py
@@ -3,12 +3,14 @@ from serial import Serial
 from time import sleep
 from subprocess import run
 
-
-# Auto detect all ports to upload, then upload to correct ports
+arduino = 'Arduino Leonardo ('
+arduino_bootloader = 'Arduino Leonardo bootloader ('
 
 while input('Press enter ') != 'quit':
-    port = ("Arduino" in comports())
-    Serial('COM'+port[:port.index(" - Arduino")],baudrate=1200).close()
-    sleep(1)
-    boot_port = ('bootloader' in comports())
-    run(["C:\\Progra~2\\Arduino\\hardware\\tools\\avr/bin/avrdude", "-CC:\\Progra~2\\Arduino\\hardware\\tools\\avr/etc/avrdude.conf", "-v", "-patmega32u4", "-cavr109", "-P"+boot_port[:boot_port.index(" - Arduino Leonardo bootloader")], "-b57600", "-D", "-Uflash:w:tclab_v2.ino.hex:i"])
+    current_ports = comports()
+    open_ports = [i.device for i in current_ports if arduino in i.description]
+    for com in open_ports:
+        Serial(com,baudrate=1200).close()
+        sleep(1)
+        upload_port = [i.device for i in comports() if arduino_bootloader in i.description]
+        run(["C:\\Progra~2\\Arduino\\hardware\\tools\\avr/bin/avrdude", "-CC:\\Progra~2\\Arduino\\hardware\\tools\\avr/etc/avrdude.conf", "-v", "-patmega32u4", "-cavr109", "-P"+upload_port[0], "-b57600", "-D", "-Uflash:w:tclab_v2.ino.hex:i"])
