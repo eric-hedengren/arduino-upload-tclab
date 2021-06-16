15dd48585339977aa3cd6d610b97db50f9df461d
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2021-06-16 13:26:54 -0600

    fixed unplug bug (loop)

--- Upload.py
+++ Upload.py
@@ -1,10 +1,12 @@
 import subprocess
+from time import time
 from serial import Serial
 from serial.tools.list_ports import comports
 
 
 def bootloader_wanted():
-    while True:
+    start = time()
+    while time()-start < 1:
         for port in comports():
             if port.description.startswith(open_device):
                 return port.device
@@ -23,7 +25,7 @@ def bootloader_unwanted():
 
 avrdude_program = 'C:\\Progra~2\\Arduino\\hardware\\tools\\avr/bin/avrdude'
 config_location = '-CC:\\Progra~2\\Arduino\\hardware\\tools\\avr/etc/avrdude.conf'
-flash_file = '-Uflash:w:tclab_v2.ino.hex:i'
+flash_file = '-Uflash:v:tclab_v2.ino.hex:i'
 
 upload_command = [avrdude_program,config_location,None,'-patmega32u4','-cavr109','-b57600','-D',flash_file]
 
@@ -32,6 +34,7 @@ open_device = 'Arduino Leonardo bootloader ('
 
 port_parameter = '-P{port}'
 
+print('Ready for a device')
 
 while True:
     for port in comports():
@@ -39,6 +42,10 @@ while True:
             Serial(port.device,baudrate=1200).close()
 
             upload_port = bootloader_wanted()
+
+            if upload_port == None:
+                print('bootloader search timed out')
+                continue
 
             upload_command[2] = port_parameter.format(port=upload_port)
             subprocess.check_output(upload_command,shell=True)
