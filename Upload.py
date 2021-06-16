50597e2454305c264caf117543d8a755034bc84c
Eric Hedengren <46898294+eric-hedengren@users.noreply.github.com>
2021-06-16 09:47:42 -0600

    reformat baby

--- Upload.py
+++ Upload.py
@@ -19,11 +19,8 @@ def bootloader_search(wanted):
                     port_list.append(port.description)
             
             if port_list == []:
-                break
+                return
 
-
-device = 'Arduino Leonardo ('
-open_device = 'Arduino Leonardo bootloader ('
 
 avrdude_program = 'C:\\Progra~2\\Arduino\\hardware\\tools\\avr/bin/avrdude'
 config_location = '-CC:\\Progra~2\\Arduino\\hardware\\tools\\avr/etc/avrdude.conf'
@@ -31,12 +28,15 @@ flash_file = '-Uflash:w:tclab_v2.ino.hex:i'
 
 upload_command = [avrdude_program,config_location,None,'-patmega32u4','-cavr109','-b57600','-D',flash_file]
 
+device = 'Arduino Leonardo ('
+open_device = 'Arduino Leonardo bootloader ('
+
 port_parameter = '-P{port}'
 
 
 while True:
     for port in comports():
-        if port.description.startswith(device): # and the drive is clean
+        if port.description.startswith(device):
             Serial(port.device,baudrate=1200).close()
 
             upload_port = bootloader_search(True)
